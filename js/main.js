e681c1c9e9057352415a07bb90da005758b226bc
julieluangpraxay <julieluangpraxay@gmail.com>
2023-09-18 22:03:28 +0000

    Add a 'submit' event listener to the form element that:  prevents the page from refreshing when the form is submitted.  stores the form's input values in a new object.  assigns an entryId property to the new object, taken from the nextEntryId property of the data model.  increments the nextEntryId property of the data model so that if another entry is submitted later, it will receive a different entryId.  adds the new object to the beginning of the data model's array of entries.  resets the preview image's src attribute back to the placeholder image.  resets the form.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,28 @@
 const $imageInput = document.querySelector('#photoUrl');
 const $image = document.querySelector('img');
+const $form = document.querySelector('form');
 
 $imageInput.addEventListener('input', function (event) {
   $image.setAttribute('src', event.target.value);
 });
 
-const $saveButton = document.querySelector('button');
-
-$saveButton.addEventListener('submit', function (event) {
+$form.addEventListener('submit', function (event) {
   event.preventDefault();
+
+  const title = $form.elements.title.value;
+  const photoUrl = $form.elements.photoUrl.value;
+  const entryBox = $form.elements.entryBox.value;
+
+  const inputValues = {
+    entryId: data.nextEntryId,
+    title,
+    photoUrl,
+    entryBox,
+  };
+
+  data.nextEntryId++;
+  data.entries.unshift(inputValues);
+
+  $image.setAttribute('src', './images/placeholder-image-square.jpg');
+  $form.reset();
 });
